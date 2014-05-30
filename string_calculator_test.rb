35eeadbcffc5ed7979142a5ca6562a612a101064
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2014-05-30 18:12:22 +0200

    implement the calculator class which handles comma as a separator

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -2,18 +2,25 @@ require 'test/unit'
 
 class StringCalculatorTest < Test::Unit::TestCase
   def test_empty
-    assert_equal calc.add(""), 0
+    assert_equal(0, calc.add(""))
   end
 
   def test_one
-    assert_equal calc.add("1"), 1
+    assert_equal(1, calc.add("1"))
   end
 
   def test_two
-    assert_equal calc.add("1,2"), 3
+    assert_equal(3, calc.add("1,2"))
   end
 
   def calc
     Calculator.new
+  end
+end
+
+
+class Calculator
+  def add(expression)
+    expression.split(",").map(&:to_i).inject(0, :+)
   end
 end
