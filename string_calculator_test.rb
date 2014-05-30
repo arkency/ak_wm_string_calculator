2c158b5cdfdd159d3ee8bc79190f06892b4bdd19
Wiktor Mociun <voter101@gmail.com>
2014-05-30 17:57:04 +0200

    Tests for single and double

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -2,7 +2,18 @@ require 'test/unit'
 
 class StringCalculatorTest < Test::Unit::TestCase
   def test_empty
-    calc = Calculator.new
     assert_equal calc.add(""), 0
+  end
+
+  def test_one
+    assert_equal calc.add("1"), 1
+  end
+
+  def test_two
+    assert_equal calc.add("1,2"), 3
+  end
+
+  def calc
+    Calculator.new
   end
 end
