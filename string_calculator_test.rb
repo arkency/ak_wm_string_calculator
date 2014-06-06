0db5e54381adc49756026cc876612fb50a5ceab1
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2014-06-06 22:38:16 +0200

    test for lengther, multiple, optional delimiters

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -13,6 +13,7 @@ class StringCalculatorTest < Test::Unit::TestCase
     assert_add(6, "//[***]\n1***2***3***2000")
     assert_add(6, "//[]]]\n1]]2]]3]]2000")
     assert_add(6, "//[*][]]\n1]2*3")
+    assert_add(6, "//[**][]]]\n1]]2**3")
     assert_not_allowed('negatives are not allowed: -1', "1,-1")
     assert_not_allowed('negatives are not allowed: -1, -5, -7', "1,-1,2,3,4,-5,-7")
   end
